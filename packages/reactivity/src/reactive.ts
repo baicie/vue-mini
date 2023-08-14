5b6baf7c7765fcc571d82c1e40f2bc22cfbbc711
white-cie <2055572879@qq.com>
2023-08-15 00:04:51 +0800

    feat: reactive 30%

--- packages/reactivity/src/reactive.ts
+++ packages/reactivity/src/reactive.ts
@@ -1,66 +1,110 @@
-import { isObject } from '@baicie/vue-shared'
-import { mutableCollectionHandlers, mutableHandlers } from './base-handler'
+import { isObject, toRawType } from "@baicie/vue-shared";
+import { mutableCollectionHandlers, mutableHandlers } from "./base-handler";
 
 export const enum ReactiveFlags {
-  SKIP = '__v_skip',
-  IS_REACTIVE = '__v_isReactive',
-  IS_READONLY = '__v_isReadOnly',
-  IS_SHALLOW = '__v_isShallow',
-  RAW = '__v_raw',
+  SKIP = "__v_skip",
+  IS_REACTIVE = "__v_isReactive",
+  IS_READONLY = "__v_isReadOnly",
+  IS_SHALLOW = "__v_isShallow",
+  RAW = "__v_raw",
 }
 
 export interface Target {
-  [ReactiveFlags.SKIP]?: boolean
-  [ReactiveFlags.IS_REACTIVE]?: boolean
-  [ReactiveFlags.IS_READONLY]?: boolean
-  [ReactiveFlags.IS_SHALLOW]?: boolean
-  [ReactiveFlags.RAW]?: any
+  [ReactiveFlags.SKIP]?: boolean;
+  [ReactiveFlags.IS_REACTIVE]?: boolean;
+  [ReactiveFlags.IS_READONLY]?: boolean;
+  [ReactiveFlags.IS_SHALLOW]?: boolean;
+  [ReactiveFlags.RAW]?: any;
 }
 
-export const reactiveMap = new WeakMap<Target, any>()
+export const reactiveMap = new WeakMap<Target, any>();
+
+const enum TargetType {
+  INVALID = 0,
+  COMMON = 1,
+  COLLECTION = 2,
+}
+
+function targetTypeMap(rawType: string) {
+  switch (rawType) {
+    case "Object":
+    case "Array":
+      return TargetType.COMMON;
+    case "Map":
+    case "Set":
+    case "WeakMap":
+    case "WeakSet":
+      return TargetType.COLLECTION;
+    default:
+      return TargetType.INVALID;
+  }
+}
 
 export function isReadonly(value: unknown): boolean {
-  return !!(value && (value as Target)[ReactiveFlags.IS_READONLY])
+  return !!(value && (value as Target)[ReactiveFlags.IS_READONLY]);
 }
 
 export function reactive(target: Object) {
   // if
-  if (isReadonly(target))
-    return target
+  if (isReadonly(target)) return target;
   return createReactiveObject(
     target,
     false,
     mutableHandlers,
     mutableCollectionHandlers,
     reactiveMap
-    
-  )
+  );
 }
 
 export function createReactiveObject(
   target: Target,
   isReadonly: boolean,
   baseHandler: ProxyHandler<any>,
-  collectionHandler: ProxyHandler<any>,
-  proxyMap: WeakMap<Target, any>,
+  collectionHandlers: ProxyHandler<any>,
+  proxyMap: WeakMap<Target, any>
 ) {
   // reactive只能代理对象类型
   if (!isObject(target)) {
     if (__DEV__)
-      console.warn(`value cannot be made reactive: ${String(target)}`)
+      console.warn(`value cannot be made reactive: ${String(target)}`);
 
-    return target
+    return target;
   }
 
-  // 没太懂
-  if(target[ReactiveFlags.RAW] && !(isReadonly && target[ReactiveFlags.IS_REACTIVE])){
-    return target
+  // 如果该对象有值 除了只读与响应式数据返回数据本身
+  // 当target是响应式直接返回 除非在响应式对象上调用readonly
+  if (
+    target[ReactiveFlags.RAW] &&
+    !(isReadonly && target[ReactiveFlags.IS_REACTIVE])
+  ) {
+    return target;
   }
 
   // 已经是代理后的结果直接返回
-  const existingProxy = proxyMap.get(target)
-  if(existingProxy)
-    return existingProxy
+  const existingProxy = proxyMap.get(target);
+  if (existingProxy) {
+    return existingProxy;
+  }
 
-    
+  // 不合法的对象直接返回无法代理
+  const targetType = getTargetType(target);
+  if (targetType === TargetType.INVALID) {
+    return target;
+  }
+
+  const proxy = new Proxy(
+    target,
+    targetType === TargetType.COLLECTION ? collectionHandlers : baseHandler
+  );
+
+  proxyMap.set(target, proxy);
+  return proxy;
+}
+
+function getTargetType(value: Target) {
+  // skip 或者当对象不可扩展时 直接返回不合法
+  //  否则获取对象类型进行判断
+  return value[ReactiveFlags.SKIP] || !Object.isExtensible(value)
+    ? TargetType.INVALID
+    : targetTypeMap(toRawType(value));
 }
--- packages/shared/src/general.ts
+++ packages/shared/src/general.ts
@@ -1,3 +1,12 @@
 export function isObject(val: unknown): boolean {
-  return val !== null && typeof val === 'object'
+  return val !== null && typeof val === "object";
+}
+
+export const objectToString = Object.prototype.toString;
+export function toTypeString(value: unknown): string {
+  return objectToString.call(value);
+}
+
+export function toRawType(value: unknown): string {
+  return toTypeString(value);
 }
