cd46dfd67dd63b8c30117bf557e99edbc2f0cbb7
刘志伟 <2055572879@qq.com>
2023-08-22 15:32:52 +0800

    feat: publish

---
+++ scripts/publish.ts
@@ -0,0 +1,50 @@
+import { findWorkspacePackages } from "@pnpm/find-workspace-packages";
+// @ts-ignore
+import { pkgsPath } from "./paths";
+import { Project } from "@pnpm/types";
+import color from 'picocolors';
+import consola from "consola";
+import { execa } from "execa";
+
+const getWorkspacePackages = () => findWorkspacePackages(pkgsPath);
+
+async function main() {
+  const pkgs = (await getWorkspacePackages()).filter(
+    (item) => !item.manifest.private
+  ) as Project[];
+
+  await updateVersion('0.0.1', pkgs)
+
+  await publishAll(pkgs);
+}
+
+async function updateVersion(version: string,targets: Project[]) {
+  for (const target of targets) {
+    await target.writeProjectManifest({
+      ...target.manifest,
+      version,
+      publishConfig:{
+        registry:'https://registry.npmjs.org/'
+      }
+    })
+  }
+}
+
+async function publishAll(targets: Project[]) {
+  for (const target of targets) {
+    execa(
+      "pnpm publish --access public",
+      [ 
+        '--no-git-checks',
+      ],  
+      {
+        stdio: 'inherit',
+        cwd: target.dir
+      }
+    )
+  }
+}
+
+main().catch((err) => {
+  consola.error(color.red(err));
+});
